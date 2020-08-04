2738ed44c1d7334eb94e9535f2a824819c839159
Min Su Kim <kim97minsu@gmail.com>
2020-08-04 12:24:11 -0700

    created form with react hooks

--- pages/next.js
+++ pages/next.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect} from 'react'
 import Head from 'next/head'
+import Form from "react-bootstrap/Form"
 import styles from '../styles/Next.module.css'
 
 export default function Home(props) {
+  const [user, setUser] = useState([
+    {
+      username: ''
+    }
+  ])
   const [infos, setInfos] = useState([
     {
       login: '',
@@ -17,13 +23,11 @@ export default function Home(props) {
       twitter: ''
     }
   ])
-  
-  useEffect(() => {
-    getData().catch((err) => console.log('Error: ', err))
-  }, [props])
 
+  const USER_API = `https://api.github.com/users/minsu3`
+  
   async function getData() {
-    const response = await fetch(`https://api.github.com/users/minsu3`)
+    const response = await fetch(USER_API)
     const info = await response.json()
     console.log('got response back')
 
@@ -32,7 +36,7 @@ export default function Home(props) {
       name: info.name,
       id: info.id,
       html_url: info.html_url,
-      hireable: info.hireable.toString(),
+      hireable: info.hireable !== null ? info.hireable.toString() : null,
       public_repos: info.public_repos,
       location: info.location,
       blog: info.blog,
@@ -42,35 +46,62 @@ export default function Home(props) {
     setInfos(infoObject)
   }
 
+  useEffect(() => {
+    getData().catch((err) => console.log('Catch Error: ', err))
+  }, [props])
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Github Job Search</h1>
+        <h1 className={styles.title}>
+          Search for Talented Software Developers!
+        </h1>
         <h2 className={styles.title}>Username: {infos.login}</h2>
         <div className={styles.grid}>
           <a className={styles.card}>
-            <p><strong>Full Name: </strong> {infos.name}</p>
+            <p>
+              <strong>Full Name: </strong>
+              {infos.name}
+            </p>
           </a>
           <a className={styles.card}>
-            <p><strong>Job Availability: </strong>{infos.hireable ? "Currently open" : "no"}</p>
+            <p>
+              <strong>Job Availability: </strong>
+              {infos.hireable ? "Currently open" : "no"}
+            </p>
           </a>
           <a className={styles.card}>
-            <p><strong>Location: </strong>{infos.location}</p>
+            <p>
+              <strong>Location: </strong>
+              {infos.location}
+            </p>
           </a>
           <a className={styles.card}>
-            <p><strong>Github Public Repos: </strong>{infos.public_repos}</p>
+            <p>
+              <strong>Github Public Repos: </strong>
+              {infos.public_repos}
+            </p>
           </a>
           <a className={styles.card}>
-            <p><strong>Email: </strong>{infos.email ? infos.email : "not included"}</p>
+            <p>
+              <strong>Email: </strong>
+              {infos.email ? infos.email : "not included"}
+            </p>
           </a>
           <a className={styles.card}>
-            <p><strong>Professional Site: </strong>{infos.blog}</p>
+            <p>
+              <strong>Professional Site: </strong>
+              {infos.blog}
+            </p>
           </a>
           <a className={styles.card}>
-            <p><strong>Twitter Username: </strong>{infos.twitter ? infos.twitter : "No Account"}</p>
+            <p>
+              <strong>Twitter Username: </strong>
+              {infos.twitter ? infos.twitter : "No Account"}
+            </p>
           </a>
         </div>
       </main>
     </div>
-  )
+  );
 }
